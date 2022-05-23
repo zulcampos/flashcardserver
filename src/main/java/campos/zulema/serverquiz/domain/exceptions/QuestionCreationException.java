fef6fa31186cdb298f11fd8e3330c59def988f1a
Zulema Campos <zulemacampos@Zulemas-Air.fios-router.home>
2022-05-23 16:27:36 -0400

    updated QuestionCreationException

--- src/main/java/campos/zulema/serverquiz/domain/exceptions/QuestionCreationException.java
+++ src/main/java/campos/zulema/serverquiz/domain/exceptions/QuestionCreationException.java
@@ -3,6 +3,7 @@ package campos.zulema.serverquiz.domain.exceptions;
 public class QuestionCreationException extends Exception{
     public QuestionCreationException(String msg){
         super(msg);
+
     }
 
 }
